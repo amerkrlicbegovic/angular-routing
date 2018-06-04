ca183b930ce3ccad959ba404ec52d0d4ba4f8332
Amer <amer.krlicbegovic@gmail.com>
2018-06-05 00:48:13 +0200

    Implementing gourd service

--- src/app/products/product.module.js
+++ src/app/products/product.module.js
@@ -18,6 +18,8 @@ var product_edit_tags_component_1 = require('./product-edit-tags.component');
 var product_filter_pipe_1 = require('./product-filter.pipe');
 var product_service_1 = require('./product.service');
 var product_resolver_service_1 = require('./product-resolver.service');
+var product_guard_service_1 = require('./product-guard.service');
+var auth_guard_service_1 = require('../user/auth-guard.service');
 var shared_module_1 = require('../shared/shared.module');
 var ProductModule = (function () {
     function ProductModule() {
@@ -29,6 +31,7 @@ var ProductModule = (function () {
                 router_1.RouterModule.forChild([
                     {
                         path: 'products',
+                        canActivate: [auth_guard_service_1.AuthGuard],
                         children: [
                             {
                                 path: '',
@@ -42,6 +45,7 @@ var ProductModule = (function () {
                             {
                                 path: ':id/edit',
                                 component: product_edit_component_1.ProductEditComponent,
+                                canDeactivate: [product_guard_service_1.ProductEditGuard],
                                 resolve: { product: product_resolver_service_1.ProductResolver },
                                 children: [
                                     { path: '', redirectTo: 'info', pathMatch: 'full' },
@@ -63,7 +67,8 @@ var ProductModule = (function () {
             ],
             providers: [
                 product_service_1.ProductService,
-                product_resolver_service_1.ProductResolver
+                product_resolver_service_1.ProductResolver,
+                product_guard_service_1.ProductEditGuard
             ]
         }), 
         __metadata('design:paramtypes', [])
--- src/app/user/user.module.js
+++ src/app/user/user.module.js
@@ -12,6 +12,7 @@ var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
 var login_component_1 = require('./login.component');
 var auth_service_1 = require('./auth.service');
+var auth_guard_service_1 = require('./auth-guard.service');
 var shared_module_1 = require('../shared/shared.module');
 var UserModule = (function () {
     function UserModule() {
@@ -28,7 +29,8 @@ var UserModule = (function () {
                 login_component_1.LoginComponent
             ],
             providers: [
-                auth_service_1.AuthService
+                auth_service_1.AuthService,
+                auth_guard_service_1.AuthGuard
             ]
         }), 
         __metadata('design:paramtypes', [])
